61564e3df7c971cc4c239012646eececb4ecd46b
harshika07 <56705398+harshika07@users.noreply.github.com>
2022-04-06 18:30:08 +0530

    Revert "static pages ui"
    This reverts commit 36cebfc0fe8c819137ed7a1d21492bd9b8033139.

---
+++ tidy-up/src/Firebase/Config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+export const config = {
+  databaseURL: "https://medicare-077.firebaseio.com",
+  apiKey: "AIzaSyDvN_tX8bhiU6INl6JxHcG28pnLrYRlOYI",
+  authDomain: "medicare-077.firebaseapp.com",
+  projectId: "medicare-077",
+  storageBucket: "medicare-077.appspot.com",
+  messagingSenderId: "732188212385",
+  appId: "1:732188212385:web:d649381d2f56f7ed6484e0",
+  measurementId: "G-LQDX59G41J",
+};
+export const BASE_URL =
+  "https://07apollo07.github.io/report-management-system/public";
+export const CACHE_NAME = "meditrust_app";
+
